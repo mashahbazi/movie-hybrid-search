c2d25ec5ae8ea891710fbbe480e0459920a6819a
mahdishahbazi <mahdishahbazi@goodhabitz.com>
2024-04-26 20:11:01 +0200

    initial commit

---
+++ src/test/kotlin/com/moviehybridsearch/MovieHybridSearchApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.moviehybridsearch
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MovieHybridSearchApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
