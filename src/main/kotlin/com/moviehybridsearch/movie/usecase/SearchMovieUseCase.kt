8d74906dce5a26ea4ac398762035737e24ba5902
mashahbazi <mahdishahbaziraz@gmail.com>
2024-06-02 17:56:16 +0330

    use hybrid search for getting movies

--- src/main/kotlin/com/moviehybridsearch/movie/usecase/SearchMovieUseCase.kt
+++ src/main/kotlin/com/moviehybridsearch/movie/usecase/SearchMovieUseCase.kt
@@ -3,14 +3,18 @@ package com.moviehybridsearch.movie.usecase
 import com.moviehybridsearch.movie.domain.Movie
 import com.moviehybridsearch.movie.repo.MovieRepository
 import com.moviehybridsearch.movie.shared.MovieMapper
+import org.springframework.ai.embedding.EmbeddingClient
 import org.springframework.stereotype.Component
 
 @Component
 class SearchMovieUseCase(
+    private val embeddingClient: EmbeddingClient,
     private val movieRepository: MovieRepository,
     private val movieMapper: MovieMapper,
 ) {
     fun execute(term: String): Iterable<Movie> {
-        throw NotImplementedError("Not implemented")
+        val vector = embeddingClient.embed(term)
+        val embedding = "[${vector.joinToString(",")}]"
+        return movieRepository.findNearestMovies(term, embedding, 10).map { movieMapper.entityToDomain(it) }
     }
 }
